846328519799c6e1515042d7e41f431f028ece7f
Radhika Saraf <radhika.saraf@uth.tmc.edu>
2020-05-20 16:39:55 -0700

    Move from github tamu

---
+++ data/data_add.m
@@ -0,0 +1,15 @@
+load Data_ADD.mat
+
+% use data from Data.xlsx in the Sheets Drugs_mine and Proteins_mine
+
+% M is the matrix to which you wish to add data about your drugs of choice
+
+% M = load('../data/mat_*.txt');
+% [r,c] = size(M);
+%  Z = zeros(r,1);
+% new_M = [M,Z];
+% [r,c] = size(new_M);
+% Z = zeros(1,c);
+% new_M = [new_M; Z];
+
+% dlmwrite('../network/mat_*.txt', new_M, '\t');
